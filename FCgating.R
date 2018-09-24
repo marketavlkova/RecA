590810b5de05ddc304f7b038951c9a26490befd8
Marketa Vlkova <marketa.m.vlkova@gmail.com>
2018-09-24 16:02:35 +1200

    Scatter values correction changed after 1st gating to get better 2nd gates

--- FCgating.R
+++ FCgating.R
@@ -14,12 +14,15 @@ FCgating <- function(data) {
   ### extract values for rectangle gate
   fsc.min <- min(exprs(f1.data$FSC.H))
   fsc.max <- max(exprs(f1.data$FSC.H))
-  if ((log10(fsc.max) - log10(fsc.min)) > 0.5) {
-    print("minimum FSC.H corrected")
-    fsc.min <- fsc.min + 800
-  }
   ssc.min <- min(exprs(f1.data$SSC.H))
   ssc.max <- max(exprs(f1.data$SSC.H))
+  if ((log10(fsc.max) - log10(fsc.min)) > 0.5) {
+    print("scatter values for second gating corrected")
+    fsc.min <- fsc.min + 10000
+    fsc.max <- fsc.max + 100000
+    ssc.min <- ssc.min + 10000
+    ssc.max <- ssc.max + 100000
+  }
   ### create a rectangle gate based on first filtered subset
   gate.rectangle <- rectangleGate(filterId = "RemoveMess", "FSC.H" = c((fsc.min - 450), (fsc.max + 3000)), "SSC.H" = c((ssc.min - 500), (ssc.max + 8000)))
   ### make second filter based on values from preliminary filter & filter the original dataset
